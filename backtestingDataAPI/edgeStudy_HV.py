956f98c8ac71e16d69dd369c15544e5c82e628c5
Sam Kling <samtkling@gmail.com>
2022-09-12 22:42:19 -0400

    setup file opening and iteration

--- backtestingDataAPI/edgeStudy_HV.py
+++ backtestingDataAPI/edgeStudy_HV.py
@@ -1,4 +1,23 @@
 import utils as u
 
+def main():
+    u.log('Start HV Study')
 
-print('hello world')
+    hvStudy('CELH')
+
+    u.log('End HV Study')
+
+def hvStudy(ticker):
+    priceFile = u.openFile('HV_STOCK_DATA/'+ticker.upper()+'_DAILY_PRICES.json')
+    priceData = u.getJson(priceFile)
+    u.closeFile(priceFile)
+    priceArray = u.getPriceArray(priceData)
+
+
+
+    for price in priceArray:
+        print(price)
+
+
+
+main()
